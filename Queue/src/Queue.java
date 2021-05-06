60ae4d016fff2820b875b5d3ef17767e0f098f67
Gayal Seneviratne <gayals40@gmail.com>
2021-05-06 13:19:30 +0530

    Enque and print methods implemented

--- Queue/src/Queue.java
+++ Queue/src/Queue.java
@@ -1,7 +1,17 @@
 public class Queue {
     private int[] queue;
     public void enqueue(int number){
-
+        if(queue == null){
+            queue = new int[1];
+            queue[0] = number;
+        } else{
+            int[] temp = new int[queue.length+1];
+            for (int i = 0; i < queue.length; i++) {
+                temp[i] = queue[i];
+            }
+            temp[queue.length] = number;
+            queue = temp;
+        }
     }
 
     public void dequeue(){
@@ -25,7 +35,11 @@ public class Queue {
     }
 
     public void print(){
-
+        System.out.print("[");
+        for (int i = 0; i < queue.length; i++) {
+            System.out.print(queue[i]+", ");
+        }
+        System.out.println("\b\b]");
     }
 
     public boolean contains(int number){
--- Queue/src/QueueTest.java
+++ Queue/src/QueueTest.java
@@ -1,6 +1,11 @@
 public class QueueTest {
     public static void main(String[] args) {
         Queue queue = new Queue();
-
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+        queue.enqueue(40);
+        queue.enqueue(50);
+        queue.print();
     }
 }
