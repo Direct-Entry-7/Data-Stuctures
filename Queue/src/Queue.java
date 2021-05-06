953a294334e19af60f77813543be45ce29deb682
Gayal Seneviratne <gayals40@gmail.com>
2021-05-06 13:03:35 +0530

    Queue class initilized

---
+++ Queue/src/Queue.java
@@ -0,0 +1,36 @@
+public class Queue {
+    private int[] queue;
+    public void enqueue(int number){
+
+    }
+
+    public void dequeue(){
+
+    }
+
+    public boolean empty(){
+        return false;
+    }
+
+    public int peek(){
+        return 0;
+    }
+
+    public void clear(){
+
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public void print(){
+
+    }
+
+    public boolean contains(int number){
+        return false;
+    }
+
+
+}
--- Stack/src/Stack.java
+++ Stack/src/Stack.java
@@ -2,62 +2,79 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Stack {
-    private Stack item;
-    ArrayList<Integer> stack = new ArrayList<>();
-    private int size;
+
+    private int[] numbers;
 
     public void push(int number){
-        stack.add(number);
-        size++;
+        if (numbers == null){
+            numbers = new int[1];
+            numbers[0] = number;
+        }else{
+            int[] temp = new int[numbers.length + 1];
+            for (int i = 0; i < numbers.length; i++) {
+                temp[i] = numbers[i];
+            }
+            temp[temp.length - 1] = number;
+            numbers = temp;
+        }
     }
 
     public void pop(){
-        if(size > 0){
-            stack.remove(size-1);
-            size--;
+        if (numbers == null){
+            System.err.println("Can't pop from empty array");
+            return;
+        }else if (numbers.length == 1){
+            numbers = null;
+            return;
         }
-
+        int[] temp = new int[numbers.length - 1];
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = numbers[i];
+        }
+        numbers = temp;
     }
 
     public boolean empty(){
-        if(size > 0)
-            return false;
-        else
-            return true;
+        return numbers == null;
+    }
+
+    public int size(){
+        return empty() ? 0 :  numbers.length;
     }
 
     public int peek(){
-        if(size > 0){
-            int peek = stack.get(size-1);
-            return peek;
-        }else{
-            return -1;
+        if (empty()){
+            System.err.println("Stack is empty");
         }
+        return numbers[numbers.length - 1];
     }
 
     public void clear(){
-        stack.clear();
+        numbers = null;
     }
 
     public void print(){
-        System.out.println(Arrays.toString(stack.toArray()));
+        if (empty()){
+            System.out.println("[]");
+            return;
+        }
+        System.out.print("[");
+        for (int i = numbers.length - 1; i >=0 ; i--) {
+            System.out.print(numbers[i] + ", ");
+        }
+        System.out.println("\b\b]");
     }
 
-
-
-
-
-    public static void main(String[] args) {
-        Stack stack = new Stack();
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
-        stack.push(40);
-        stack.push(50);
-        stack.print();
-        stack.pop();
-        stack.print();
-
-
+    public boolean contains(int number){
+        if (empty()){
+            return false;
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == number){
+                return true;
+            }
+        }
+        return false;
     }
+
 }
